129f31140d7649111e72737859b119a7f78ec4a3
AMIKAM-AI-INVESTOR <ofiramikam@gmail.com>
2025-06-08 16:06:07 +0300

    Delete graph_engine.py

--- graph_engine.py
+++
@@ -1,9 +0,0 @@
-# modules/graph_engine.py
-import streamlit as st
-import plotly.graph_objs as go
-
-def render_graph(symbol, df):
-    fig = go.Figure()
-    fig.add_trace(go.Candlestick(x=df.index, open=df['Open'], high=df['High'],
-                                 low=df['Low'], close=df['Close'], name=symbol))
-    st.plotly_chart(fig, use_container_width=True)
